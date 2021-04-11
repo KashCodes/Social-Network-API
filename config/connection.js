b7b5d72840daf539e8d342802de96dfec98bdb10
Kash <kashay@agyeman.me>
2021-04-11 13:28:03 -0700

    folders and connection.js started

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose"); 
+
+mongoose.connect("mongodb://localhost/socialmedia", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection
+
+
